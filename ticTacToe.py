1033d95fd0b53f0ebf38a698e95a6b4d806c205c
faraja17 <farajakthompson@gmail.com>
2022-08-03 22:47:09 -0400

    combined the spot filled and keyerror input validations
    created an array containg all of the valid moves (playerBoard keys) used the if in statement to loop through the array. if it contains the players move and the key value is a hyphen, it changes the hyphen to either x or o and increments i. otherwise it gives the blanket invalid move statement. the continue causes the the loop to restart without checking for a winner or switching players. I am THRILLED!!!

--- ticTacToe.py
+++ ticTacToe.py
@@ -4,6 +4,8 @@ theBoard = {'tl': '-', 'tm': '-', 'tr': '-',
             'ml': '-', 'mm': '-', 'mr': '-',
             'll': '-', 'lm': '-', 'lr': '-'}
 
+keys = ['tl', 'tm', 'tr', 'ml', 'mm', 'mr', 'll', 'lm', 'lr']
+
 # wecome message and directions
 print('Welcome to Tic Tac Toe!')
 print('Object of the game: To win, get three Xs or Os in a row, column, or diagonal.')
@@ -24,82 +26,65 @@ turn = 'X'
 
 
 # players enter moves
-def playerTurn(move, turn):
+def playerTurn(move, turn, theBoard):
 
     i = 0
 
-    while i < 9:
+    while i < 9: #changed this from for to while loop. this prevents counter from counting invalid moves
         printBoard(theBoard)
         print(i)
         print(turn + ', it is your turn. Select a spot.')
         move = input()
 
-        # j = 0
-
-        # while True: 
-        #     try:
-        #         for j in range(9):
-        #             if move == keys[j]:
-        #                 break
-        #             else:    
-        #                 j += 1
-        #     except KeyError:
-        #         print("Invalid move. Try again.")
-        #         continue
-        #     else:
-        #         break  
-        
-
-        if theBoard[move] == '-':
+        if move in keys and theBoard[move] == '-':
             theBoard[move] = turn
             i += 1
-        
-        else:
-            print("That space is already taken. Select an empty spot.")
-            continue
-             
-
-    #     if turn == 'X':
-    #         turn = 'O'
-    #     else:
-    #         turn = 'X'
             
-    # drawMessage()
+        else:
+            print("Invalid move. Try again.")
+            continue #this skips everthing below and goes back up to the beginning of the loop. player turn remains the same.
 
     # determinine and announce winner
 
         firstLetterCount = 0
         secondLetterCount = 0
+        
+        #This checks for row wins:
         for k in theBoard.keys():
-            if (k[0] == move[0]) and (theBoard[k] == turn): 
+            if (k[0] == move[0]) and (theBoard[k] == turn): #k[0] is the first letter of the key. move[0] is the first letter of the player's selection theBoard[k] is the key value. turn is the X or O.
+                print("same in row:")
                 print(k[0], move[0], theBoard[k], turn)
                 firstLetterCount+=1       
-            if firstLetterCount == 3: 
+            if firstLetterCount == 3: #There is only one instance when the first letters of three spots are the same. That is in either row t, m, or l.
                 winMessage(turn)
             else:
                 continue  
-
+        
+        #This checks for column wins:
         for k in theBoard.keys():
-            if (k[1] == move[1]) and (theBoard[k] == turn):
+            if (k[1] == move[1]) and (theBoard[k] == turn):#k[1] is the second letter of the key. move[1] is the second letter of the player's selection theBoard[k] is the key value. turn is the X or O,
+                print("same in column:")
                 print(k[1], move[1], theBoard[k], turn)
                 secondLetterCount+=1
-            if secondLetterCount == 3:
+            if secondLetterCount == 3: #There is only one instance when the second letters of three spots are the same. That is in either column l, m, or r.
                 winMessage(turn)
             else:
                 continue
         
+        #These check for diagonal wins:
         if (theBoard['tl'] == turn) and (theBoard['mm'] == turn) and (theBoard['lr'] == turn):
             winMessage(turn)
 
         if (theBoard['tr'] == turn) and (theBoard['mm'] == turn) and (theBoard['ll'] == turn): 
             winMessage(turn)
 
+        #This swaps the player after each turn:
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
         
-        playerTurn("move", turn) # takes back into game play if there is no winner.
+        # Restarts game play if there is no winner.
             
     drawMessage()
 
@@ -118,4 +103,4 @@ def drawMessage():
     exit()
 
 
-playerTurn("move", turn) #starts the game
+playerTurn("move", turn, theBoard) #starts the game
